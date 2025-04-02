687a3885a0f1c5f9f9da099ab661a3402b23dfda
Stefanie Kröll <110983291+stkroe@users.noreply.github.com>
2025-04-02 14:38:03 +0200

    change otter exercise

---
+++ course/examples/notebooks/ex1/.tests/q8.py
@@ -0,0 +1,15 @@
+OK_FORMAT = True
+
+test = {   'name': 'q8',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': '>>> def test_loops_conditions(out):\n'
+                                               '...     output = out.splitlines()\n'
+                                               "...     assert output == ['2', '4', '6', '8']\n"
+                                               '>>> test_loops_conditions(out)\n',
+                                       'hidden': True,
+                                       'locked': False,
+                                       'points': 2}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
